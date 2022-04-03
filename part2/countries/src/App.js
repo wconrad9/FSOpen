1659fec408e2ef650643214575e968291e1ae26a
Walter <wconrad@middlebury.edu>
2022-04-03 17:01:17 -0600

    finished countries app

---
+++ part2/countries/src/App.js
@@ -0,0 +1,130 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+import axios from 'axios'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const CapitalWeather = ({ capital }) => {
+  const [weatherData, setWeatherData] = useState(null)
+
+  // replace API key!
+  let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&APPID=4a72b0cb213687ed3cae899d8ebe7c47`
+  
+  useEffect(() => {
+    axios
+    .get(endpoint)
+    .then( response => {
+      setWeatherData(response.data)
+    })
+  }, [])
+  
+  if (weatherData !== null) {
+    const icon = weatherData.weather[0].icon
+    return (
+      <>
+      <p>temperature {(weatherData.main.temp-273.15).toFixed(2)} celsius</p>
+      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={"weather icon"}/>
+      <p>wind: {weatherData.wind.speed} m/s</p>
+      </>
+    )
+  }
+}
+
+const CountryView = ({country}) => {
+  const languages = country.languages
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>capital: {country.capital}</p>
+      <p>land area: {country.area}</p>
+      <h3>languages:</h3>
+      <ul>
+        {Object.keys(languages).map( k => <li key={k}>{languages[k]}</li>)}
+      </ul>
+      <img src={country.flags.png} alt="flag"/>
+      <h1>Weather in {country.capital}</h1>
+      <CapitalWeather capital={country.capital}/>
+      </div>
+  )
+}
+
+const Countries = ({ countries }) => {
+
+  const[selectedCountry, setSelectedCountry] = useState(null)
+
+
+  const showCountry = (c) => {
+    setSelectedCountry(c)
+  }
+
+  if (selectedCountry !== null) {
+    return (
+      <>
+      <CountryView country={selectedCountry}/>
+      <Button text={"back"} onClick={() => setSelectedCountry(null)}/>
+      </>
+    )
+  }
+
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter.</p>
+    )
+  } else if (countries.length <= 10 && countries.length > 1) {
+    return (
+      <ul>
+      {countries.map( c => {
+        return (
+        <li key={c.name.common}>
+          {c.name.common}
+          <Button text={"show"} onClick={() => showCountry(c)}/>
+        </li>
+        )
+      })}
+      </ul>
+    )
+  } else if (countries.length === 0) {
+    return
+  } else {
+    return (
+      <>
+        <CountryView country={countries[0]}/>
+      </>
+    )
+  }
+}
+
+function App() {
+  const [countryFilter, setCountryFilter] = useState('')
+  const [countryList, setCountryList] = useState([])
+
+  const filterCountries = (event) => {
+    const newFilter = event.target.value.toLowerCase()
+    setCountryFilter(newFilter)
+
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then( response => {
+        const countries = response.data
+        const filteredCountries = countries.filter( (country) => {
+          const name = country.name.common.toLowerCase()
+          return name.includes(newFilter)
+        }
+        )
+        setCountryList(filteredCountries)
+      })
+  }
+
+  return (
+    <div>
+      find countries <input value={countryFilter} onChange={filterCountries}/>
+      {countryFilter ? <Countries countries={countryList}/> : null}
+    </div>
+  );
+}
+
+export default App;
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 const Filter = ({onChange}) => {
@@ -37,15 +38,19 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then( response => {
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -59,6 +64,7 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1
     }
     const newPersons = persons.concat(newPerson)
     setPersons(newPersons)
